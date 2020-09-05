2d3470b2acb581f5e5631b0a27b414bc9180e148
TechFallMacBook <techfallmacbook@MacBook-Pro.home>
2020-09-05 15:38:20 -0400

    almost finished, frustrated

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 let friends = ["Neeraj", "WayneCarl", "Bobbo", "The Piker", "The Stain"]
 
-/**
-for (name of friends) {
-    for (let x=99; x>0; x--){
-        if (x>2){
-            console.log(`${x} lines of code in the file, ${x} lines of code; ${name} strikes one out, clears it all out, ${x-1} lines of code in the file`)
-        }else if (x=2){
-            console.log(`${x} line of code in the file, ${x} line of code; ${name} strikes it out, clears it all out, ${x-1} line of code in the file`)
-        }else {
-            console.log(`${x} line of code in the file, ${x} line of code; ${name} strikes it out, clears it all out, ${x-1} lines of code in the file`)
-        }
-    }
-}
-**/
+document.getElementById('theButton').addEventListener('click',()=>{
+    for (name of friends){
+        let div = document.createElement('div')
+        let h3 = document.createElement('h3')
+        let h3Text = document.createTextNode(name)
+        let p = document.createElement('p')
+        let pText
+        document.body.appendChild(div)
+        div.appendChild(h3)
+        h3.appendChild(h3Text)
+        let br = document.createElement('br')
+        
+        
 
-for (name of friends){
-    for (let x=99; x>0; x--){
-        if (x>2){
-            console.log(`${x} lines of code in the file, ${x} lines of code; ${name} strikes one out, clears it all out, ${x-1} lines of code in the file`)
-        }else if (x===2){
-            console.log(`${x} line of code in the file, ${x} line of code; ${name} strikes it out, clears it all out, ${x-1} line of code in the file`)
-        }else {
-            console.log(`${x} line of code in the file, ${x} line of code; ${name} strikes it out, clears it all out, ${x-1} lines of code in the file`)
+        for (let x=99; x>0; x--){
+            div.appendChild(p) //Why isn't this working? it's only creating 1 paragraph element. I want it to create one for each line.
+            if (x>2){
+                pText = document.createTextNode(`${x} lines of code in the file, ${x} lines of code; ${name} strikes one out, clears it all out, ${x-1} lines of code in the file`)
+            }else if (x===2){
+                pText = document.createTextNode(`${x} lines of code in the file, ${x} lines of code; ${name} strikes it out, clears it all out, ${x-1} line of code in the file`)
+            }else {
+                pText = document.createTextNode(`${x} line of code in the file, ${x} line of code; ${name} strikes it out, clears it all out, ${x-1} lines of code in the file`)
+            }
+            p.appendChild(pText)
+            div.appendChild(br)
         }
     }
-}
+})
+
